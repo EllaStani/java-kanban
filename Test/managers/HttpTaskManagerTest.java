1b927460d8f8fffc4596697decbf691d396a769e
EllaShimanskaya <shimanskayaella@yandex.ru>
2022-09-07 00:21:52 +0300

    remove extra import

--- Test/managers/HttpTaskManagerTest.java
+++ Test/managers/HttpTaskManagerTest.java
@@ -1,6 +1,5 @@
 package managers;
 
-import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
